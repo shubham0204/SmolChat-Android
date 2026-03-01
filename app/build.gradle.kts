40eae70feb9872f4390e8a068067285dde7e6d51
Shubham Panchal <panchalshubhamdev@gmail.com>
2026-03-01 14:45:53 +0530

    release: prepare for release v14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "io.shubham0204.smollmandroid"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "13"
+        versionCode = 14
+        versionName = "14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
